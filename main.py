a4c47b6853a05d14bbf6ab9f73deebbf87c01746
albi <alberto.battisti1@gmail.com>
2024-05-27 15:55:06 +0200

    added user switch selector, more colours, blinking
    User switch selector 4 modes, added more colours depending on the
    temperature, added blinking speed depending on the air quality, removed
    onboard led for heater status,

--- main.py
+++ main.py
@@ -2,27 +2,21 @@
 The intensity and the blinking pattern of the light change in relation with the air temperature and humidity"""
 
 from inventor import Inventor2040W, NUM_LEDS
-from machine import Pin
 from time import sleep
 from breakout_bme68x import BreakoutBME68X, STATUS_HEATER_STABLE
 from pimoroni_i2c import PimoroniI2C
 
 
+#Already set some LEDs to change colour with the temperature measured by the bme680
+#every 10 loops it prints the measurements of the sensor
+#goal for today: create a blinking pattern dependent on the air quality
 
-
-#initialize the board 
+#hardware initialization
 board = Inventor2040W()
-
-#initialize led on Pico
-pico_led = Pin("LED", Pin.OUT)
-#CNST
-DELAY = .2
-
-
-# Create a bme connected via i2c to breakout pins
 PINS_BREAKOUT_GARDEN = {"sda": 4, "scl": 5}
 i2c = PimoroniI2C(**PINS_BREAKOUT_GARDEN)
 bme = BreakoutBME68X(i2c)
+
 
 def read_sensor_data(sensor_instance):
     """remember to initialize board and sensor before calling this function
@@ -31,49 +25,91 @@ def read_sensor_data(sensor_instance):
     temperature, pressure, humidity, gas, status, *_ = sensor_instance.read()
     heater = "Stable" if status & STATUS_HEATER_STABLE else "Unstable"
 
+
 def print_sensor_data(temp, pres, hum, gs, heat):
-    """prints neatly formatted data, read_sensor_data() must be called before"""
+    """prints neatly formatted data, 'read_sensor_data()' must be called before"""
     print(f"{temp:0.2f}c, {pres:0.2f}Pa, {hum:0.2f}%, {gs:0.2f} Ohms, Heater: {heat}")
 
-def blink_temperature_leds(temp):
+
+def blinking_speed(gs):
+    """Based on the gas measurement, increase or decrease the speed of the blinking pattern"""
+    value = gs/1000
+    if value < 50:
+        sleep_cnst = .2
+    elif 50 <= value < 75:
+        sleep_cnst = .5
+    elif 75 <= value < 100:
+        sleep_cnst = 1
+    elif 100 <= value:
+        sleep_cnst = 5
+    return float(sleep_cnst)
+
+
+def number_leds(counter_press):
+    """uses the count of the user switch presses to return the number of leds to be used
+    in the 'blink_temperature_leds()"""
+    if counter_press % 4 == 0:
+        leds = NUM_LEDS - 6
+    elif counter_press % 4 == 1:
+        leds = NUM_LEDS
+    elif counter_press % 4 == 2:
+        leds = NUM_LEDS - 9
+    elif counter_press % 4 == 3:
+        leds = NUM_LEDS - 3
+    return int(leds)
+    
+
+def blink_temperature_leds(speed_function_blink, numleds, temp):
+    """Sets 6 of the 12 LED's on the inventor board into a blinking pattern,
+    the colour depends on the temperature measured by the temperature sensor
+    The blinking speed depends on 'blinking_pattern()' """
     if 20.00 <= temp < 25.00:
-            for i in range(0, NUM_LEDS - 6):
+            for i in range(0, numleds):
                 board.leds.set_rgb(i, green_r, green_g, green_b)
     elif 18.00 <= temp < 20.00:
-        for i in range(0, NUM_LEDS -6):
+        for i in range(0, numleds):
             board.leds.set_rgb(i, blue_r, blue_g, blue_b)
     elif 25.00 <= temp < 27.00:
-        for i in range(0, NUM_LEDS - 6):
+        for i in range(0, numleds):
             board.leds.set_rgb(i, orange_r, orange_g, orange_b)
-    sleep(.2)
-    for i in range(0, NUM_LEDS - 6):
-        board.leds.set_rgb(i, 0, 0, 0)
+    elif 27.00 <= temp < 30.00:
+        for i in range(0, numleds):
+            board.leds.set_rgb(i, red_r, red_g, red_b)
+    elif 30.00 < temp:
+        for i in range(0, numleds):
+            board.leds.set_rgb(i, purple_r, purple_g, purple_b)
+    # IF THE AIR IS NOT POLLUTED, DON'T BLINK AT ALL
+    if speed_function_blink != 5:
+        sleep(speed_function_blink)
+        for i in range(0, numleds):
+            board.leds.set_rgb(i, 0, 0, 0)
+        sleep(speed_function_blink)
+
 
 
 ## COLOURS
-red_r, red_b, red_g = 255, 0, 0
+red_r, red_g, red_b = 255, 0, 0
 orange_r, orange_g, orange_b = 249, 154, 14
 green_r, green_g, green_b = 0, 255, 0
 blue_r, blue_g, blue_b = 0, 0, 255
+purple_r, purple_g, purple_b = 255, 0, 255
 
-
-count = 0
+#initialize counters
+count_iterations = 0
+user_switch_count = 0
 
 while True:
     try:
         read_sensor_data(bme)
-        if (count % 10) == 0:
+        #print the sensor data just every 10th iteration
+        if (count_iterations % 10) == 0:
             print_sensor_data(temperature, pressure, humidity, gas, heater)
-        blink_temperature_leds(temperature)
-        if heater == "Stable":
-            pico_led.toggle()
-        elif heater == "Unstable":
-            pico_led.on()
-        sleep(DELAY)
-        count += 1
+        count_iterations += 1
+        if board.switch_pressed():
+            user_switch_count += 1
+        blink_temperature_leds(blinking_speed(gas), number_leds(user_switch_count), temperature)
     except KeyboardInterrupt:
         board.leds.clear()
-        pico_led.off()
         break
 
 
